d93c4c3ddf72f52771bd76451517bc8886f2aaa0
cyrille <cyrilleotieno7@gmail.com>
2021-12-27 17:34:05 +0300

    finish

--- src/main/silentbidding/src/components/Layout/Footer.js
+++ src/main/silentbidding/src/components/Layout/Footer.js
@@ -54,93 +54,23 @@ class Footer extends Component{
                <p className="tsa-footer-heading" >Quick Links</p>
                <ul>
                   <li>
-                     <a href="http://www.sportsauthority.com/category/index.jsp?categoryId=3158282">About Us</a>
+                     <a href="/login">Login</a>
                   </li>
                   <li>
-                     <a href="https://www.sportsauthority.com/checkout/index.jsp?process=orderTrackingLogin">Track Order</a>
+                     <a href="/signup">Signup</a>
                   </li>
-                  <li>
-                     <a href="http://stores.sportsauthority.com/">Find a Store</a>
-                  </li>
-                  <li>
-                     <a href="https://www.sportsauthority.com/checkout/index.jsp?process=login">My Account</a>
-                  </li>
-                  <li>
-                     <a href="http://www.sportsauthority.com/sitemap/index.jsp?clickid=topnav_sitemap_txt">Site Map</a>
-                  </li>
-                  <li>
-                     <a href="http://www.sportsauthority.com/category/index.jsp?categoryId=11165242">Giving Back</a>
-                  </li>
-               </ul>
-            </div>
-            <div id="tsaSpotlight" className="tsa-footer-column">
-               <p className="tsa-footer-heading">In the Spotlight</p>
-               <ul>
-                  <li>
-                     <a href="http://www.sportsauthority.com/Outlet/shop.jsp?categoryId=3267003&amp;clickid=botnav_outlet_txt">Deal &amp; Clearance</a>
-                  </li>
-                  <li>
-                     <a href="http://www.sportsauthority.com/Gift-Center/shop.jsp?categoryId=12240335">Gift Buyers Guide</a>
-                  </li>
-                  <li>
-                     <a href="http://www.sportsauthority.com/shop-by-brand/category.jsp?categoryId=12961273">Shop by Brand</a>
-                  </li>
-                  <li><a href="http://shop.sportsauthority.com/">Top Products</a>
-                  </li>
-                  <li>
-                     <a href="http://weeklyad.sportsauthority.com/flyers/sportsauthority?locale=en&amp;type=1#!/flyers/sportsauthority-weekly?flyer_run_id=64022">Weekly Ad</a>
-                  </li>
-                  <li>
-                     <a href="http://www.sportsauthority.com/shop/index.jsp?categoryId=11308301&amp;sr=1&amp;origkw=coupon&amp;view=grid">Store Locator</a>
-                  </li>
-               </ul>
-            </div>
-            <div id="tsaOurStores" className="tsa-footer-column">
-               <p className="tsa-footer-heading">Our Stores</p>
-               <ul>
                   <li>
                      <a href="http://stores.sportsauthority.com/">Find a Store</a>
                   </li>
                   <li>
-                     <a href="http://www.sportsauthority.com/groupsales/index.jsp?clickid=topnav_groupsales_txt">Group Sales</a>
+                     <a href="/account">My Account</a>
                   </li>
-                  <li>
-                     <a href="http://www.sportsauthority.com/Team-Rewards/shop.jsp?categoryId=12800810">Team Savings</a>
-                  </li>
-                  <li>
-                     <a href="http://www.sportsauthority.com/price-match-guarantee/shop.jsp?categoryId=12754202">Ad Price Match</a>
-                  </li>
-                  <li>
-                     <a href="http://www.sportsauthority.com/Store-Services/shop.jsp?categoryId=12455250">In-Store Services</a>
-                  </li>
-                  <li>
-                     <a href="http://www.sportsauthority.com/Store-Services/shop.jsp?categoryId=12455250">In-Store Rentals</a>
-                  </li>
+                  
                </ul>
             </div>
-            <div id="tsaHelpCenter" className="tsa-footer-column">
-               <p className="tsa-footer-heading">Help Center</p>
-               <ul>
-                  <li>
-                     <a href="http://www.sportsauthority.com/helpdesk/index.jsp?clickid=topnav_helpdesk_txt">Customer Service</a>
-                  </li>
-                  <li>
-                     <a href="http://www.sportsauthority.com/Returns/Returns-Exchanges/shop.jsp?categoryId=3971372">Returns &amp; Warranties</a>
-                  </li>
-                  <li>
-                     <a href="http://www.sportsauthority.com/helpdesk/availabilityPop.jsp">Pricing</a> | <a href="http://www.sportsauthority.com/helpdesk/index.jsp?display=ship&amp;subdisplay=shipMethods&amp;clickid=botnav_shipping_txt"> Shipping</a>
-                  </li>
-                  <li>
-                     <a href="http://www.sportsauthority.com/category/index.jsp?categoryId=2661943">Privacy</a> | <a href="http://www.sportsauthority.com/helpdesk/index.jsp?display=safety&amp;subdisplay=terms">Terms of Use</a>
-                  </li>
-                  <li>
-                     <a href="http://www.sportsauthority.com/Recalls/shop.jsp?categoryId=10978122">Recalls</a> | <a href="http://www.sportsauthority.com/Settlements/shop.jsp?categoryId=3429947">Settlements</a>
-                  </li>
-                  <li>
-                     <a href="http://www.sportsauthority.com/helpdesk/sb657compliance.jsp" target="_blank">SB657 Compliance</a>
-                  </li>
-               </ul>
-            </div>
+            
+            
+            
             <div id="tsaContactUs" className="tsa-footer-help">
                <div>
                   <p className="tsa-footer-heading">Need Help Online?</p>
@@ -197,12 +127,10 @@ class Footer extends Component{
          <div className="legal">
             <div className="left">
             </div>
-            <div className="right">
-               <a id="tsaDesktopSiteLink" href="http://m.sportsauthority.com/category/index.jsp?categoryId=60832076&amp;MobileOptOut=1">View Desktop Site</a>
-            </div>
+            
          </div>
          <div className="legalBig">
-            <p>&copy; 2021 silent bidding</p>
+            <p>&copy; 2021 Silent bidding.</p>
          </div>
       </div>
      </div>
